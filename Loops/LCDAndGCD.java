453980da746048ecc49fa039cd8df154c3fa9b4d
Furkan Patat <119625055+furkanpatat@users.noreply.github.com>
2023-08-08 16:14:52 +0300

    Add files via upload

---
+++ Loops/LCDAndGCD.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LCDAndGCD {
+    public static void main(String[] args) {
+
+        //We are writing a program that finds the GCD
+        // and LCM values of two numbers in Java.
+
+        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int n1, n2,i = 0,ebob = 1,ekok;
+
+
+        System.out.print("Enter the first Number : ");
+        n1 = scanner.nextInt();
+
+        System.out.print("Enter the second Number  :");
+        n2 = scanner.nextInt();
+
+        while (i < n2){
+            i++;
+            if (n1 % i == 0 && n2 % i == 0){
+                ebob = i;
+            }
+        }
+        ekok = (n1 * n2) / ebob;
+        System.out.println("Greatest Common Divisor : " + ebob);
+        System.out.println("Lowest Common Multiple : "+ekok);
+    }
+}
